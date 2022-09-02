cc40530b5bc97ab0971bf5ff200b21811df9ab93
Leonardo Rosas <leonrosant92@gmail.com>
2022-09-02 11:31:51 -0500

    :tada: First commit

---
+++ src/views/About.vue
@@ -0,0 +1,8 @@
+<template>
+    <main>
+        <img src="https://picsum.photos/800/300" alt="Placeholder">
+        <h1>About</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed modi error, voluptate labore qui reiciendis.</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Exercitationem nisi perferendis at, alias distinctio quisquam rem. Nulla deleniti, voluptates ab voluptate sit consequatur accusantium accusamus fugiat sed nam sint ut?</p>
+    </main>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,7 @@
+<template>
+    <main>
+        <h1>Home</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora nisi eos porro? Soluta minima itaque dolore?</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Exercitationem nisi perferendis at, alias distinctio quisquam rem. Nulla deleniti, voluptates ab voluptate sit consequatur accusantium accusamus fugiat sed nam sint ut?</p>
+    </main>
+</template>
